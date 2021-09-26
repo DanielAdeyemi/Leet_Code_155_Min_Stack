04659c3f0b85f2d415aeb6b8981f40e8fc5b58f2
DanielAdeyemi <adeyemidany@gmail.com>
2021-09-25 19:36:39 -0700

    second solution using extra array

--- index.js
+++ index.js
@@ -42,3 +42,48 @@ MinStack.prototype.top = function () {
 MinStack.prototype.getMin = function () {
 	return this.stack[this.stack.length - 1][1];
 };
+
+// Second solution with additional stack
+
+var MinStack = function () {
+	this.stack = [];
+	this.min = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+	this.stack.push(val);
+	if (this.min[this.min.length - 1] >= val || this.min.length == 0) {
+		this.min.push(val);
+	}
+	return;
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+	this.stack.pop();
+	let x = this.stack.pop();
+	if (this.min[this.min.length - 1] == x) this.min.pop();
+	return;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+	if (this.stack.length == 0) return null;
+	return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+	if (this.min.length == 0) return null;
+	return this.min[this.min.length - 1];
+};
