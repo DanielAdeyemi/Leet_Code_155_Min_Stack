486a0069d363daaaa874486bff489579a8ecc575
DanielAdeyemi <adeyemidany@gmail.com>
2021-09-25 22:45:42 -0700

    add append to min only for mins/smallest and pop only if it's min value

--- functions.py
+++ functions.py
@@ -6,14 +6,13 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if len(self._min) == 0 or (self._min[len(self._min) - 1] > val):
+        if len(self._min) == 0 or (self._min[len(self._min) - 1] >= val):
             self._min.append(val)
-        else:
-            self._min.append(self._min[len(self._min) - 1])
 
     def pop(self) -> None:
-        self.stack.pop()
-        self._min.pop()
+        x = self.stack.pop()
+        if x == self.getMin():
+            self._min.pop()
 
     def top(self) -> int:
         return self.stack[len(self.stack) - 1]
